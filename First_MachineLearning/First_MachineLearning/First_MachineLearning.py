208d4da63f93e84dc7784e45cf2bffeb5b3cedcf
gratiaQ <cafe43@naver.com>
2019-07-14 07:41:08 +0900

    test

---
+++ First_MachineLearning/First_MachineLearning/First_MachineLearning.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv('C:/Users/Q/Desktop/ETC/basketball_stat.csv')
+
+df.head()
+
+print('ab')
---
+++ test/test.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv("C:/Users/Q/Desktop/ETC/basketball_stat.csv")
+
+print(df.head())
